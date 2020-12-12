532de020454ab81c8356171933ed335ed9bd96f1
vladislav <ve@incsub.com>
2020-12-12 18:02:57 +0100

    Refactor styles and output

--- cli.js
+++ cli.js
@@ -1,15 +1,7 @@
-const util = require('util');
-const fs = require('fs');
-const readFile = util.promisify(fs.readFile);
-
 const capture = require('./lib/capture.js');
 const preview = require('./lib/preview.js');
 const styles = require('./lib/styles.js');
-
-const listStyles = async () => console.log(
-	(await styles.getAll())
-		.reduce((prev, style) => `${prev}${style}\n`, '')
-);
+const output = require('./lib/output.js');
 
 const main = async () => {
 	const args = [...process.argv].slice(2);
@@ -19,7 +11,7 @@ const main = async () => {
 	);
 	if (subcommand.length) {
 		return 'ls' === subcommand[0]
-			? listStyles()
+			? styles.list()
 			: preview();
 	}
 
@@ -27,21 +19,13 @@ const main = async () => {
 		(prev, arg) => arg.match(/--style=/) ? arg.replace(/--style=/, '') : prev,
 	'');
 	const stdout = args.filter(arg => '--' === arg).length;
-	const file = args.filter(arg => arg !== '--').reduce(
-		(prev, arg) => ! arg.match(/--style=/) ? arg.replace(/--style=/, '') : prev,
+	const file = args.filter(arg => !arg.match(/^--/)).reduce(
+		(prev, arg) => arg || prev,
 	0 /* 0 means stdin */);
 
 	const path = await capture.file(file, style);
 	if (stdout) {
-		const img = await readFile(path);
-		const { exec } = require('child_process');
-		exec(`xclip -sel c -t image/png -i ${path}`, { timeout: 1000 }, err => {
-			if (err) {
-				console.error(err);
-				process.exit(1);
-			}
-			fs.unlinkSync(path);
-		});
+		return output.clipboard(path)
 	}
 };
 main();
