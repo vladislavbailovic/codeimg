3611901c55b67943c7a85edeae610a5f5dc4b3e2
vladislav <ve@incsub.com>
2020-12-12 19:03:44 +0100

    Refactor command line options

--- cli.js
+++ cli.js
@@ -1,31 +1,66 @@
+const cliArgs = require('command-line-args');
+const usage = require('command-line-usage');
+
 const capture = require('./lib/capture.js');
 const preview = require('./lib/preview.js');
 const styles = require('./lib/styles.js');
 const output = require('./lib/output.js');
 
-const main = async () => {
-	const args = [...process.argv].slice(2);
+const optionDefinitions = [
+	{ name: 'file', description: 'File containing code', alias: 'f', type: String, defaultOption: true },
+	{ name: 'style', description: 'Stylesheet to use', alias: 's', type: String },
+	{ name: 'output', description: 'Output file - use dash for stdout, dot to just append png. Default is clipboard', alias: 'o', type: String },
+	{ name: 'base64', description: 'Base64-encode stdout', alias: 'b', type: Boolean },
+	{ name: 'help', description: 'Show this help', alias: 'h', type: Boolean },
+];
+const commandDefinitions = [
+	{ name: 'command', description: 'Subcommand - one of help, ls or serve', defaultOption: true, typeLabel: '(optional)' },
+];
 
-	const subcommand = args.filter(
-		arg => 'ls' === arg || 'serve' === arg
-	);
-	if (subcommand.length) {
-		return 'ls' === subcommand[0]
-			? styles.list()
-			: preview();
+const help = () => console.log(usage([
+	{ header: 'Code to image (c2i)', content: 'Converts textual code to an image' },
+	{ header: 'Subcommands', optionList: commandDefinitions },
+	{ header: 'Options', optionList: optionDefinitions },
+]));
+
+const main = async () => {
+	const subcommands = cliArgs(commandDefinitions, { stopAtFirstUnknown: true });
+	const rawCommand = subcommands.command;
+	const command = [ 'ls', 'search', 'help' ].reduce((prev, cmd) => rawCommand === cmd ? cmd : prev, 'capture')
+
+	let file = 'capture' === command && !rawCommand
+		? 0
+		: rawCommand;
+
+	if ('ls' === command) {
+		return styles.list();
 	}
 
-	const style = args.reduce(
-		(prev, arg) => arg.match(/--style=/) ? arg.replace(/--style=/, '') : prev,
-	'');
-	const stdout = args.filter(arg => '--' === arg).length;
-	const file = args.filter(arg => !arg.match(/^--/)).reduce(
-		(prev, arg) => arg || prev,
-	0 /* 0 means stdin */);
+	if ('serve' === command) {
+		return preview();
+	}
 
-	const path = await capture.file(file, style);
-	return stdout
-		? output.clipboard(path)
-		: output.file(path, file);
+	if ('help' === command) {
+		return help();
+	}
+
+	const argv = subcommands._unknown || []
+	const options = cliArgs(optionDefinitions, { argv });
+	if (options.help) {
+		return help();
+	}
+	
+	if (options.file) {
+		file = options.file;
+	}
+
+	const path = await capture.file(file, options.style);
+	if (!options.output) {
+		return output.clipboard(path);
+	}
+
+	return options.output === "-"
+		? output.stdout(path, options.base64)
+		: output.file(path, options.output === "." ? file : options.output);
 };
 main();
